e8969f94750b8bcaecc9fbf3fb3f1aa5acf33338
Jack Jolliffe <jolliffejack@gmail.com>
2022-09-20 11:13:08 -0400

    Finished tests

--- Pet.cs
+++ Pet.cs
@@ -7,7 +7,47 @@ using System.Threading.Tasks;
 namespace template_csharp_virtual_pet
 {
     public class Pet
-    {
+       
 
+    {
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public int Hunger { get; set; }
+        public int Boredom { get; set; }
+        public int Health { get; set; }
+
+        public Pet()
+        {
+            Name = "Jackson";
+            Species = "Dog";
+            Hunger = 60;
+            Boredom = 60;
+            Health = 60;  
+
+	    }
+
+        public void Feed()
+        {
+            Hunger -= 10;
+	    }
+        
+        public void SeeDoctor()
+        {
+            Health += 30;
+	    }
+
+        public void Play()
+        {
+            Hunger += 10;
+            Boredom -= 20;
+            Health += 10;
+	    }
+
+        public void Tick()
+        {
+            Hunger += 5;
+            Boredom += 5;
+            Health -= 5;
+	    }
     }
 }
